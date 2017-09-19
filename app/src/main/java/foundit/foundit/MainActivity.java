ad09f7e02572288d1d88a6c874c7101544f910e3
Anterion <Anterion69@gmail.com>
2017-09-20 00:29:18 +0200

    Commit de prueba 1

--- app/src/main/java/foundit/foundit/MainActivity.java
+++ app/src/main/java/foundit/foundit/MainActivity.java
@@ -2,12 +2,27 @@ package foundit.foundit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView texto;
+    private Button boton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boton = (Button) findViewById(R.id.button2);
+        texto = (TextView) findViewById(R.id.eT1);
+
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                texto.setText("Hello World");
+            }
+        });
     }
 }
