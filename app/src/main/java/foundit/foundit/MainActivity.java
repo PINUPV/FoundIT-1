4c90bc82473ea566c5a7046605f778513f40b0a4
antorfe1 <32105637+antorfe1@users.noreply.github.com>
2017-10-10 18:29:06 +0200

    He cambiado el  "Hola Mundo Cruel" por "Hola Mundo Cruel de mierda num"

--- app/src/main/java/foundit/foundit/MainActivity.java
+++ app/src/main/java/foundit/foundit/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
         boton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                texto.setText("Hola Mundo Cruel de mierda");
+                texto.setText("Hola Mundo Cruel de mierda 2345698745");
             }
         });
         boton.setText("Biches 2");
