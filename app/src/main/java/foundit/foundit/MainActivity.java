39d9c33d5bfc9186bdb4161c02fdfbbde21e460a
Anterion <Anterion69@gmail.com>
2017-10-10 18:27:35 +0200

    añadido texto nuevo en el boton

--- app/src/main/java/foundit/foundit/MainActivity.java
+++ app/src/main/java/foundit/foundit/MainActivity.java
@@ -24,5 +24,6 @@ public class MainActivity extends AppCompatActivity {
                 texto.setText("Hola Mundo Cruel de mierda");
             }
         });
+        boton.setText("Biches 2");
     }
 }
