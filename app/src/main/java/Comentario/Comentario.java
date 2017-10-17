bce024ecd90b11b36b1c840cd3dad1f89d1170d7
nathankiller <nathancole94@hotmail.com>
2017-10-17 18:18:49 +0200

    added constructors && gets&sets to some of the domain classes

--- app/src/main/java/Comentario/Comentario.java
+++ app/src/main/java/Comentario/Comentario.java
@@ -1,8 +1,46 @@
 package Comentario;
 
+import Comercio.Comercio;
+import Usuario.Usuario;
+
 /**
  * Created by nathan on 10/10/2017.
  */
 
 public class Comentario {
+
+    Usuario usuario;
+    Comercio comercio;
+    String text;
+
+    public Comentario(Usuario usuario, Comercio comercio, String text)
+    {
+        this.usuario = usuario;
+        this.comercio = comercio;
+        this.text = text;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Comercio getComercio() {
+        return comercio;
+    }
+
+    public void setComercio(Comercio comercio) {
+        this.comercio = comercio;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
--- app/src/main/java/Comercio/Comercio.java
+++ app/src/main/java/Comercio/Comercio.java
@@ -1,12 +1,102 @@
 package Comercio;
 
+import android.support.annotation.NonNull;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
 /**
  * Created by nathan on 10/10/2017.
  */
 
-public class Comercio
-{
-    private String name, address, city, zipcode, tlf;
+public class Comercio {
 
 
+    private String name, address, city, country, email, webAdress;
+    private int zipcode, tlf;
+    private List<String> Categorias;
+
+    public Comercio() {
+        name = "";
+        address = "";
+        city = "";
+        country = "";
+        email = "";
+        webAdress = "";
+        zipcode = 0;
+        tlf = 0;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getWebAdress() {
+        return webAdress;
+    }
+
+    public void setWebAdress(String webAdress) {
+        this.webAdress = webAdress;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public int getTlf() {
+        return tlf;
+    }
+
+    public void setTlf(int tlf) {
+        this.tlf = tlf;
+    }
+
+    public List<String> getCategorias() {
+        return Categorias;
+    }
+
+    public void setCategorias(List<String> categorias) {
+        Categorias = categorias;
+    }
 }
