54c68abdce8a1ef98948483d7583a78aefaa53f8
Anterion <Anterion69@gmail.com>
2017-10-26 01:36:49 +0200

    Reparado el FragRegistro_Usuario.

--- app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
+++ app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
@@ -17,68 +17,76 @@ import android.support.v7.app.AppCompatActivity;
 import static foundit.foundit.R.id.text_contraseña;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FragRegistro_Usuario extends AppCompatActivity {
+public class FragRegistro_Usuario extends Fragment {
 
+
+    public FragRegistro_Usuario(){
+
+    }
 
     EditText contraseña, nombre, apellidos, usuario, confircontra, correo;
+    Button bTNRegUserAlta;
+
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState){
         // Inflate the layout for this fragment
-       // return inflater.inflate(R.layout.fragment_frag_registro__usuario, container, false);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_frag_registro__usuario);
-        contraseña = (EditText) findViewById(R.id.text_contraseña);
-        nombre = (EditText) findViewById(R.id.text_nombre);
-        apellidos = (EditText) findViewById(R.id.text_apellidos);
-        usuario = (EditText) findViewById(R.id.text_usuario);
-        confircontra = (EditText) findViewById(R.id.text_confircontra);
-        correo = (EditText) findViewById(R.id.text_correo);
-    }
-    public void darDeAlta(View v){
-        if(comprobar_contraseña(contraseña.getText().toString()) && comprobar_nombre(nombre.getText().toString()) &&
-                comprobar_apellidos(apellidos.getText().toString()) && comprobar_usuario(usuario.getText().toString()) &&
-                comprobar_confircontra(contraseña.getText().toString(), confircontra.getText().toString()) &&
-                comprobar_correo(correo.getText().toString())) {
-            Intent Main = new Intent(getApplicationContext(), MainFoundit.class);
-            startActivity(Main);
-        }
+        View view = inflater.inflate(R.layout.fragment_frag_registro__usuario, container, false);
 
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_frag_registro__usuario);
+        contraseña = (EditText) view.findViewById(R.id.text_contraseña);
+        nombre = (EditText) view.findViewById(R.id.text_nombre);
+        apellidos = (EditText) view.findViewById(R.id.text_apellidos);
+        usuario = (EditText) view.findViewById(R.id.text_usuario);
+        confircontra = (EditText) view.findViewById(R.id.text_confircontra);
+        correo = (EditText) view.findViewById(R.id.text_correo);
+        bTNRegUserAlta=(Button) view.findViewById(R.id.bTNRegUserAlta);
+        bTNRegUserAlta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(comprobar_contraseña(contraseña.getText().toString()) && comprobar_nombre(nombre.getText().toString()) &&
+                        comprobar_apellidos(apellidos.getText().toString()) && comprobar_usuario(usuario.getText().toString()) &&
+                        comprobar_confircontra(contraseña.getText().toString(), confircontra.getText().toString()) &&
+                        comprobar_correo(correo.getText().toString())) {
+                    Intent Main = new Intent(getActivity(), MainFoundit.class);
+                    startActivity(Main);
+                }
+            }
+        });
 
-
+        return view;
     }
     public boolean comprobar_contraseña(String contra){
         if(contra.length() < 6) {
-            Toast.makeText(FragRegistro_Usuario.this, "La contraseña debe tener 6 dígitos", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "La contraseña debe tener 6 dígitos", Toast.LENGTH_LONG).show();
             return false;
         }
         else return true;
     }
     public boolean comprobar_nombre(String nombre) {
         if (nombre.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_apellidos(String apellidos) {
         if (apellidos.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_usuario(String usuario) {
         if (usuario.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_confircontra(String contra1, String contra2) {
         if (!contra1.equals(contra2)) {
-            Toast.makeText(FragRegistro_Usuario.this, "Las contraseñas deben ser iguales", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Las contraseñas deben ser iguales", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
@@ -86,7 +94,7 @@ public class FragRegistro_Usuario extends AppCompatActivity {
         if (!TextUtils.isEmpty(correo) && android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
             return true;
         } else {
-            Toast.makeText(FragRegistro_Usuario.this, "Introduce una dirección de correo válida", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Introduce una dirección de correo válida", Toast.LENGTH_LONG).show();
             return false;
         }
     }
