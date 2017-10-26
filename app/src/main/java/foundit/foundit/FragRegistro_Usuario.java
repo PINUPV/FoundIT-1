22ade5d01888e34c654c404e2b98b91839f8b239
erica <ericaog8@gmail.com>
2017-10-26 16:33:25 +0200

    Falta la asynctask

--- app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
+++ app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
@@ -2,6 +2,8 @@ package foundit.foundit;
 
 
 import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -14,71 +16,88 @@ import android.widget.EditText;
 import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 
+import java.io.IOException;
+
 import static foundit.foundit.R.id.text_contraseña;
 
 
-/**
- * A simple {@link Fragment} subclass.
- */
-public class FragRegistro_Usuario extends AppCompatActivity {
+public class FragRegistro_Usuario extends Fragment {
 
 
-    EditText contraseña, nombre, apellidos, usuario, confircontra, correo;
+    public FragRegistro_Usuario(){
+
+    }
+
+    EditText contraseña, nombre, apellidos, usuario, confircontra, correo, poblacion;
+    Button bTNRegUserAlta;
+
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,
+                             Bundle savedInstanceState){
         // Inflate the layout for this fragment
-       // return inflater.inflate(R.layout.fragment_frag_registro__usuario, container, false);
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_frag_registro__usuario);
-        contraseña = (EditText) findViewById(R.id.text_contraseña);
-        nombre = (EditText) findViewById(R.id.text_nombre);
-        apellidos = (EditText) findViewById(R.id.text_apellidos);
-        usuario = (EditText) findViewById(R.id.text_usuario);
-        confircontra = (EditText) findViewById(R.id.text_confircontra);
-        correo = (EditText) findViewById(R.id.text_correo);
-    }
-    public void darDeAlta(View v){
-        if(comprobar_contraseña(contraseña.getText().toString()) && comprobar_nombre(nombre.getText().toString()) &&
-                comprobar_apellidos(apellidos.getText().toString()) && comprobar_usuario(usuario.getText().toString()) &&
-                comprobar_confircontra(contraseña.getText().toString(), confircontra.getText().toString()) &&
-                comprobar_correo(correo.getText().toString())) {
-            Intent Main = new Intent(getApplicationContext(), MainFoundit.class);
-            startActivity(Main);
-        }
+        View view = inflater.inflate(R.layout.fragment_frag_registro__usuario, container, false);
 
+        //super.onCreate(savedInstanceState);
+        //setContentView(R.layout.fragment_frag_registro__usuario);
+        contraseña = (EditText) view.findViewById(R.id.text_contraseña);
+        nombre = (EditText) view.findViewById(R.id.text_nombre);
+        apellidos = (EditText) view.findViewById(R.id.text_apellidos);
+        usuario = (EditText) view.findViewById(R.id.text_usuario);
+        confircontra = (EditText) view.findViewById(R.id.text_confircontra);
+        correo = (EditText) view.findViewById(R.id.text_correo);
+        poblacion = (EditText) view.findViewById(R.id.text_poblacion);
+        bTNRegUserAlta=(Button) view.findViewById(R.id.bTNRegUserAlta);
+        bTNRegUserAlta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(comprobar_nombre(nombre.getText().toString()) && comprobar_apellidos(apellidos.getText().toString()) &&
+                        comprobar_usuario(usuario.getText().toString()) &&
+                        comprobar_contraseña(contraseña.getText().toString()) &&
+                        comprobar_confircontra(contraseña.getText().toString(), confircontra.getText().toString()) &&
+                        comprobar_correo(correo.getText().toString())) {
+                   String x = "http://185.137.93.170:8080/registro-usuario.php?alias=" + usuario.getText() +
+                           "&email=" + correo.getText() + "&pass=" + contraseña.getText() + "&poblacion=" + poblacion.getText() +
+                           "&provincia=null&pais=null&cp=null&telefono=null"));
+                    Toast.makeText(getActivity(), x, Toast.LENGTH_LONG).show();
+                    //Intent Main = new Intent(getActivity(), MainFoundit.class);
+                   // startActivity(Main);
 
+                }
+            }
+        });
 
+        return view;
     }
     public boolean comprobar_contraseña(String contra){
         if(contra.length() < 6) {
-            Toast.makeText(FragRegistro_Usuario.this, "La contraseña debe tener 6 dígitos", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "La contraseña debe tener 6 dígitos", Toast.LENGTH_LONG).show();
             return false;
         }
         else return true;
     }
     public boolean comprobar_nombre(String nombre) {
         if (nombre.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_apellidos(String apellidos) {
         if (apellidos.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_usuario(String usuario) {
         if (usuario.length() == 0) {
-            Toast.makeText(FragRegistro_Usuario.this, "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Rellenar todos los campos obligatorios", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
     public boolean comprobar_confircontra(String contra1, String contra2) {
         if (!contra1.equals(contra2)) {
-            Toast.makeText(FragRegistro_Usuario.this, "Las contraseñas deben ser iguales", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Las contraseñas deben ser iguales", Toast.LENGTH_LONG).show();
             return false;
         } else return true;
     }
@@ -86,8 +105,23 @@ public class FragRegistro_Usuario extends AppCompatActivity {
         if (!TextUtils.isEmpty(correo) && android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()) {
             return true;
         } else {
-            Toast.makeText(FragRegistro_Usuario.this, "Introduce una dirección de correo válida", Toast.LENGTH_LONG).show();
+            Toast.makeText(getActivity(), "Introduce una dirección de correo válida", Toast.LENGTH_LONG).show();
             return false;
         }
     }
+}
+
+    private class RegisterTask extends AsyncTask<String, String, Boolean> {
+        String result;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+
+            result = Util.GetWeb(Uri.parse(params[0]));
+
+            return true;
+        }
+
+        @Override
+        protected void onP
 }
--- app/src/main/java/foundit/foundit/Util.java
+++ app/src/main/java/foundit/foundit/Util.java
@@ -1,36 +1,159 @@
 package foundit.foundit;
 
-import java.io.BufferedInputStream;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
-import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 /**
  * Created by Yop-Portatil on 25/10/2017.
  */
 
 public class Util {
-    public static String GetWeb(HttpURLConnection urlConnection) {
+    public static String GetWeb(Uri uri) {
         StringBuilder out = new StringBuilder("");
         String line;
         InputStream in = null;
         try {
-            in = urlConnection.getInputStream();
+            URL url = new URL(uri.toString());
+            Log.v("debug", url.toString());
+            in = url.openConnection().getInputStream();
+
             BufferedReader dis = new BufferedReader(new InputStreamReader(in));
             while ((line = dis.readLine()) != null) {
                 out.append(line);
             }
             return out.toString();
         } catch (IOException e) {
-            e.printStackTrace();
+            Log.e("ERROR2", e.toString());
         } finally {
             if (in != null) try {
                 in.close();
             } catch (IOException e) { }
         }
         return "";
+    }
+static double radius = 0;
+    public static void CargarComerciosEnMapa(GoogleMap mMap, String busqueda) {
+        String lat = Double.toString(mMap.getCameraPosition().target.latitude);
+        String lon = Double.toString(mMap.getCameraPosition().target.longitude);
+        int maxZoomLevel = 21;
+        radius = (10 * Math.pow(2, maxZoomLevel - mMap.getCameraPosition().zoom)/*"500"*/);
+        Log.e("Zoom", ""+mMap.getCameraPosition().zoom);
+        Log.e("Radius", ""+radius);
+        Uri uri = new Uri.Builder().scheme("http")
+                .encodedAuthority("185.137.93.170:8080")
+                .path("busqueda.php")
+                .appendQueryParameter("distancia", "" + radius)
+                .appendQueryParameter("gpslat", lat)
+                .appendQueryParameter("gpslong", lon)
+                .appendQueryParameter("busqueda", busqueda)
+                .appendQueryParameter("filtro", "[]")
+                .build();
+        ActualizaMapa am = new ActualizaMapa();
+        am.mMap = mMap;
+        am.execute(uri);
+    }
+}
+
+class ActualizaMapa extends AsyncTask<Uri, Void, JSONObject> {
+    public GoogleMap mMap;
+    public JSONArray resultadosAnteriores = new JSONArray();
+    private Exception exception;
+    static ArrayList<MarkerCache> oldMarkers = new ArrayList<MarkerCache>();
+
+    protected JSONObject doInBackground(Uri... urls) {
+        Uri uri = urls[0];
+        String received = Util.GetWeb(uri);
+        Log.e("Recibido", received);
+        JSONObject obj = null;
+        try {
+            obj = new JSONObject(received);
+        } catch (JSONException e) {
+            Log.e("ERROR JSONException", e.toString());
+        } catch (Exception e) {
+            Log.e("ERROR general", e.toString());
+        }
+        return obj;
+    }
+
+    class MarkerCache {
+        Marker marker;
+        String ID;
+        public MarkerCache(String ID, Marker marker) {
+            this.marker = marker;
+            this.ID = ID;
+        }
+    }
+
+    protected void onPostExecute(JSONObject respuesta) {
+        synchronized (oldMarkers) {
+            int antes = oldMarkers.size();
+            int borrados = 0;
+            int nuevos = 0;
+            int oldRecycled = 0;
+            ArrayList<MarkerCache> newMarkers = new ArrayList<MarkerCache>();
+            try {
+                if (respuesta.get("resultado").equals("ok")) {
+                    JSONArray array = respuesta.getJSONArray("mensaje");
+                    Log.e("Cantidad de marcadores", "" + array.length());
+                    for (int i = 0; i < array.length(); i++) {
+                        JSONObject obj = array.getJSONObject(i);
+
+                        MarkerCache oldMarker = idEnAnteriores(obj.getString("ID"));
+                        if (oldMarker == null) {
+                            LatLng latlong = new LatLng(Double.parseDouble(obj.getString("Latitud")), Double.parseDouble(obj.getString("Longitud")));
+                            newMarkers.add(new MarkerCache(obj.getString("ID"), mMap.addMarker(
+                                    new MarkerOptions()
+                                    .position(latlong)
+                                    .title(obj.getString("Nombre"))
+                                    .snippet(obj.getString("Calle")))));
+                            nuevos++;
+                        } else {
+                            newMarkers.add(oldMarker);
+                            oldMarkers.remove(oldMarker);
+                            oldRecycled++;
+                        }
+                    }
+                }
+            } catch (JSONException e) {
+                Log.e("Error JSON", e.toString());
+                e.printStackTrace();
+            }
+
+            for (int i = 0; i < oldMarkers.size(); i++){
+                oldMarkers.get(i).marker.remove();
+                borrados++;
+            }
+            oldMarkers.clear();
+            oldMarkers.addAll(newMarkers);
+            // Radio capturado
+            //mMap.addCircle(new CircleOptions().center(mMap.getCameraPosition().target).radius(Util.radius));
+        }
+    }
+
+    private MarkerCache idEnAnteriores(String id) throws JSONException {
+        for (int i = 0; i < oldMarkers.size(); i++) {
+            if (oldMarkers.get(i).ID.equals(id)) {
+                return oldMarkers.get(i);
+            }
+        }
+        return null;
     }
 }
