c788a10352c92393862fff0da49013b1fab6410c
Érica Olmo <ericaog8@gmail.com>
2017-11-15 11:07:41 +0100

    icono de like sin fondo

--- app/src/main/java/foundit/foundit/frag_favoritos.java
+++ app/src/main/java/foundit/foundit/frag_favoritos.java
@@ -3,9 +3,11 @@ package foundit.foundit;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Spinner;
@@ -28,11 +30,13 @@ public class frag_favoritos extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_frag_favoritos, null, false);
-        Spinner spinner = (Spinner) view.findViewById(R.id.spinner);
-        String[] filtro = {"Proximidad","Fecha"};
-        spinner.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, filtro));
+        Spinner spinner = (Spinner) view.findViewById(R.id.FavSpinner);
+        String[] filtro = new String[] {"Proximidad","Fecha"};
+        ArrayAdapter<String> adapter =(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, filtro));
+        spinner.setAdapter(adapter);
 
-        return inflater.inflate(R.layout.fragment_frag_favoritos, container, false);
+
+        return view;
 
     }
 
