72a7f0e11c238537dd40966bbbdb31e0c438178e
alpasgui <albert1451991@gmail.com>
2017-11-18 20:51:48 +0100

    Ficha comercio con funcionalidad - Falta probar.

--- app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
+++ app/src/main/java/foundit/foundit/FragRegistro_Usuario.java
@@ -7,30 +7,18 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import android.support.v7.app.AppCompatActivity;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
-
-import static foundit.foundit.ActualizaMapa.oldMarkers;
-import static foundit.foundit.R.id.text_contraseña;
 
 
 public class FragRegistro_Usuario extends Fragment {
--- app/src/main/java/foundit/foundit/Fragficha_comercio.java
+++ app/src/main/java/foundit/foundit/Fragficha_comercio.java
@@ -2,24 +2,23 @@ package foundit.foundit;
 
 
 import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableContainer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.design.widget.NavigationView;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.RatingBar;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.concurrent.ExecutionException;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -30,6 +29,11 @@ public class Fragficha_comercio extends Fragment {
     Button bt_cerrar_ficha;
     ImageButton bt_like;
     RatingBar rbarTotal, rbarComercio;
+    Float valoracion;
+    int IDUsuario = 22;
+    int IDComercio = 2204;
+    String comentario = "";
+    Boolean yaValorado = false;
 
     public Fragficha_comercio() {
         // Required empty public constructor
@@ -43,16 +47,28 @@ public class Fragficha_comercio extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_fragficha_comercio, container, false);
 
+        onFichaOpen();
+
         bt_cerrar_ficha = (Button) view.findViewById(R.id.bt_cerrar_fichaCom);
         bt_cerrar_ficha.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View v){
                 Intent Main = new Intent(getActivity(), MainFoundit.class);
                 startActivity(Main);
             }
         });
 
-
+        rbarComercio = (RatingBar) view.findViewById(R.layout.fragment_fragficha_comercio);
+        rbarComercio.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener(){
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                if(!yaValorado){
+                puntuar(rating);}
+                else{
+                    Toast.makeText(getActivity(), "Ya has valorado este comercio", Toast.LENGTH_LONG);
+                }
+            }
+        });
         bt_like = (ImageButton) view.findViewById(R.id.bt_like);
         bt_like.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,5 +77,75 @@ public class Fragficha_comercio extends Fragment {
             }
         });
         return view;
+    }
+
+    private void onFichaOpen() {
+        String x = "http://185.137.93.170:8080/sql.php?sql=185.137.93.170:8080/sql.php?sql=SELECT * FROM Comentarios WHERE IDUsuario = "+IDUsuario+"AND IDComercio = "+IDComercio;
+        RegisterTaskFicha t = new RegisterTaskFicha();
+        t.faF = getActivity();
+        try {
+            JSONObject respuesta = t.execute(x).get();
+            if (respuesta.toString().contains("ok")){
+            yaValorado = true;
+            }else{yaValorado = false;}
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+       }
+
+    private void puntuar(float val ) {
+        String x = "http://185.137.93.170:8080/sql.php?sql=185.137.93.170:8080/sql.php?sql=INSERT INTO Comentarios(ID, IDUsuario, IDComercio, IDComentResponse, ComentText, Rate) " +
+                "VALUES (0,"+IDUsuario+","+IDComercio+",null,"+comentario+","+val+")";
+
+        RegisterTaskFicha t = new RegisterTaskFicha();
+        t.faF = getActivity();
+        //Toast.makeText(getActivity(), x, Toast.LENGTH_LONG).show();
+        try {
+            JSONObject respuesta = t.execute(x).get();
+            if (respuesta.toString().contains("ok")){
+                Toast.makeText(getActivity(), "Comercio puntuado con un: "+val, Toast.LENGTH_LONG).show();
+              // refreshRating();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        
+
+    }
+
+    private void refreshRating() {
+
+    }
+}
+class RegisterTaskFicha extends AsyncTask<String, String, JSONObject> {
+
+    FragmentActivity faF;
+    @Override
+    protected JSONObject doInBackground(String... params) {
+        String result = Util.GetWeb(Uri.parse(params[0]));
+        try {
+            return new JSONObject(result);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        try {
+            return new JSONObject("{\"resultado\":\"error\",\"mensaje\":\"No se ha podido realizar la accion\"}");
+        } catch (JSONException e) {
+            return null; // Nunca ocurrirá
+        }
+    }
+
+    @Override
+    protected void onPostExecute(JSONObject respuesta) {
+        try {
+            Toast.makeText(faF,respuesta.getString("mensaje"),Toast.LENGTH_SHORT).show();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
