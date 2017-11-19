1ddfc527837aacbc8615b4e45d491845363dec16
alpasgui <albert1451991@gmail.com>
2017-11-19 20:06:21 +0100

    Ficha comercio con funcionalidad - No guarda las valoraciones

--- app/src/main/java/foundit/foundit/Fragficha_comercio.java
+++ app/src/main/java/foundit/foundit/Fragficha_comercio.java
@@ -19,8 +19,10 @@ import android.widget.Toast;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.Marker;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.concurrent.ExecutionException;
 
@@ -34,7 +36,7 @@ public class Fragficha_comercio extends Fragment {
     ImageButton bt_like;
     RatingBar rbarTotal, rbarComercio;
     Float valoracion;
-    int IDUsuario = 22;
+    int IDUsuario = 23;
     int IDComercio = 2204;
     String comentario = "";
     Boolean yaValorado = false;
@@ -74,7 +76,7 @@ public class Fragficha_comercio extends Fragment {
                 if(!yaValorado){
                 puntuar(rating);}
                 else{
-                    Toast.makeText(getActivity(), "Ya has valorado este comercio", Toast.LENGTH_LONG);
+                    Toast.makeText(getActivity(), "Ya has valorado este comercio", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -90,12 +92,12 @@ public class Fragficha_comercio extends Fragment {
 
     private void onFichaOpen() {
         //IDComercio = Integer.parseInt(marker.getId().replaceAll("[^0-9]", ""));
-        String x = "http://185.137.93.170:8080/sql.php?sql=SELECT%20*%20FROM%20Comentarios%20WHERE%20IDUsuario%20=%2022%20AND%20IDComercio%20=%202204";
+        String x = "http://185.137.93.170:8080/sql.php?sql=SELECT%20*%20FROM%20Comentarios%20WHERE%20IDUsuario%20=%20"+IDUsuario+"%20AND%20IDComercio%20=%20"+IDComercio;
         RegisterTaskFicha t = new RegisterTaskFicha();
         t.faF = getActivity();
         try {
-            JSONObject respuesta = t.execute(x).get();
-            if (respuesta.toString().contains("ok")){
+            JSONArray respuesta = t.execute(x).get();
+            if (respuesta.toString().contains(String.valueOf(IDUsuario))){
             yaValorado = true;
             }else{yaValorado = false;}
         } catch (InterruptedException e) {
@@ -106,18 +108,19 @@ public class Fragficha_comercio extends Fragment {
        }
 
     private void puntuar(float val ) {
-        String x = "http://185.137.93.170:8080/sql.php?sql=INSERT INTO Comentarios(ID, IDUsuario, IDComercio, IDComentResponse, ComentText, Rate) " +
-                "VALUES (0,"+IDUsuario+","+IDComercio+",null,"+comentario+","+val+")";
+        String x = "http://185.137.93.170:8080/sql.php?sql=INSERT%20INTO%20Comentarios(ID,%20IDUsuario,%20IDComercio,%20IDComentResponse,%20ComentText,%20Rate)" +
+                "%20VALUES(null,"+IDUsuario+","+IDComercio+",null,"+comentario+","+val+")";
 
         RegisterTaskFicha t = new RegisterTaskFicha();
         t.faF = getActivity();
         //Toast.makeText(getActivity(), x, Toast.LENGTH_LONG).show();
         try {
-            JSONObject respuesta = t.execute(x).get();
-            if (respuesta.toString().contains("ok")){
-                Toast.makeText(getActivity(), "Comercio puntuado con un: "+val, Toast.LENGTH_LONG).show();
+
+            JSONArray respuesta = t.execute(x).get();
+
+                Toast.makeText(getActivity(), "Comercio puntuado con un: "+val, Toast.LENGTH_SHORT).show();
               // refreshRating();
-            }
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
@@ -132,30 +135,31 @@ public class Fragficha_comercio extends Fragment {
 
     }
 }
-class RegisterTaskFicha extends AsyncTask<String, String, JSONObject> {
+class RegisterTaskFicha extends AsyncTask<String, String, JSONArray> {
 
     FragmentActivity faF;
     @Override
-    protected JSONObject doInBackground(String... params) {
+    protected JSONArray doInBackground(String... params) {
         String result = Util.GetWeb(Uri.parse(params[0]));
+
         try {
-            return new JSONObject(result);
+           return new JSONArray(result);
         } catch (JSONException e) {
             e.printStackTrace();
         }
         try {
-            return new JSONObject("{\"resultado\":\"error\",\"mensaje\":\"No se ha podido realizar la accion\"}");
+            return new JSONArray("{\"resultado\":\"error\",\"mensaje\":\"No se ha podido realizar la accion\"}");
         } catch (JSONException e) {
             return null; // Nunca ocurrirá
         }
     }
 
     @Override
-    protected void onPostExecute(JSONObject respuesta) {
-        try {
-            Toast.makeText(faF,respuesta.getString("mensaje"),Toast.LENGTH_SHORT).show();
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+    protected void onPostExecute(JSONArray respuesta) {
+       // try {
+          //  Toast.makeText(faF,respuesta.get(0).toString(),Toast.LENGTH_SHORT).show();
+       // } catch (JSONException e) {
+       //     e.printStackTrace();
+       // }
     }
 }
